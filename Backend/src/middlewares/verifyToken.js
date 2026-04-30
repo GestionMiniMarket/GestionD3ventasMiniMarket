3fcec08ed4c61120229088ccf9a750abed8e08a4
Dennys Lozano <dennyricardo1405@gmail.com>
2026-04-30 01:35:43 -0500

    Se configuro la seguridad para verificar tokens JWT

---
+++ Backend/src/middlewares/verifyToken.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader) {
+    return res.status(403).json({ mensaje: 'Token requerido' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(403).json({ mensaje: 'Token inválido' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.usuario = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ mensaje: 'Token inválido' });
+  }
+};
+
+module.exports = verifyToken;
