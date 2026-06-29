179a3c03ebc8da267033e9b0eca0fbea229c72da
JuanJoMatias <jjams170105@gmail.com>
2026-06-29 03:53:00 -0500

    Agrega servicios de cajas ventas y reportes

---
+++ frontend/src/services/cajasService.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/api/cajas";
+
+const getAuthHeader = () => ({
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
+});
+
+export const abrirCaja = (monto_inicial) => {
+  return axios.post(API_URL + "/abrir", { monto_inicial }, getAuthHeader());
+};
+
+export const getResumenCaja = () => {
+  return axios.get(API_URL + "/resumen", getAuthHeader());
+};
+
+export const registrarEgreso = (data) => {
+  return axios.post(API_URL + "/egreso", data, getAuthHeader());
+};
+
+export const solicitarCierre = (efectivo_contado) => {
+  return axios.put(API_URL + "/solicitar-cierre", { efectivo_contado }, getAuthHeader());
+};
+
+export const listarCajas = () => {
+  return axios.get(API_URL, getAuthHeader());
+};
+
+export const confirmarCierre = (id, observaciones) => {
+  return axios.put(`${API_URL}/${id}/confirmar-cierre`, { observaciones }, getAuthHeader());
+};
---
+++ frontend/src/services/reportesService.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/api/reportes";
+
+const getAuthHeader = () => ({
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
+});
+
+export const getDashboardReporte = () => {
+  return axios.get(`${API_URL}/dashboard`, getAuthHeader());
+};
+
+export const getVentasDiarias = (fecha = "") => {
+  const query = fecha ? `?fecha=${fecha}` : "";
+  return axios.get(`${API_URL}/ventas/diarias${query}`, getAuthHeader());
+};
+
+export const getVentasSemanales = () => {
+  return axios.get(`${API_URL}/ventas/semanales`, getAuthHeader());
+};
+
+export const getVentasMensuales = (mes, anio) => {
+  return axios.get(`${API_URL}/ventas/mensuales?mes=${mes}&anio=${anio}`, getAuthHeader());
+};
+
+export const getProductosMasVendidos = () => {
+  return axios.get(`${API_URL}/productos/mas-vendidos`, getAuthHeader());
+};
+
+export const getProductosBajaRotacion = () => {
+  return axios.get(`${API_URL}/productos/baja-rotacion`, getAuthHeader());
+};
+
+export const getEgresosPorCaja = (id) => {
+  return axios.get(`${API_URL}/cajas/${id}/egresos`, getAuthHeader());
+};
